e16c134c770bc5eaed140182cdd56b18c6af46cb
eneiqmaneiro <henriquetacou@gmail.com>
2026-09-10 23:12:02 -0300

    feat : Criando lista de produtos e recuperando estes dados no componente atravès de métodos de array.

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -1,5 +1,29 @@
 export default function Home() {
+
+    //Exemplo de destructuring
+    const estojo = {
+        nome: "Estojo",
+        cor: "Azul",
+        tamanho: 10
+    }
+
+    //Acessando os dados do objeto diretamente
+    console.log(estojo.nome)
+    console.log(estojo.cor)
+    console.log(estojo.tamanho)
+
+    //Desestruturando o objeto (Destructuring)
+    const {nome, cor, tamanho} = estojo;
+
     return (
-        <div></div>
+        <main>
+            <h2>Home</h2>
+            <div>
+                <h3>Estojo</h3>
+                <p>Nome: {nome}</p>
+                <p>Cor: {cor}</p>
+                <p>Tamanho: {tamanho}</p>
+            </div>
+        </main>
     )
 } 
