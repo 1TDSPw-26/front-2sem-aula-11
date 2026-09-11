8564968d5bfea0b9718a0baed3dd138cf4ce60ae
Matsushita1907 <theus.matsu@gmail.com>
2026-09-10 23:11:36 -0300

    feat: Criando lista de produtos e recuperando estes dados no componente através de métodos de array

---
+++ my-app/src/components/Imagens/index.tsx
@@ -0,0 +1,13 @@
+export default function Imagens() {
+  return (
+    <div>
+        <figure>
+          <img
+            src="https://placehold.co/300x200/0000FF/FFFFFF/png"
+            alt="Figure de demonstração"
+          />
+          <figcaption>Imagem Externa</figcaption>
+        </figure>
+    </div>
+  )
+}
